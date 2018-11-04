798e91e30d5bcb3472b14912b4162d59beebacc4
Jam Ding <jdbadassnewbie326@gmail.com>
2018-11-04 23:32:48 +0800

    second files upload

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
-   res.sendFile('index.html', {root: path.join(__dirname, './')})
+   res.sendFile('index.html', {root: path.join(__dirname, './public')})
 });
 
 app.listen(8080, function(){
